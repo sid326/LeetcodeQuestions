5cbd52eec984ea2476874fb24c2d360b82467aea
sid326 <sid326@users.noreply.github.com>
2021-04-06 23:50:21 +0530

    Merge 2 list

---
+++ Easy/21. Merge Two Sorted Lists/Merge2List.java
@@ -0,0 +1,55 @@
+import java.util.PriorityQueue;
+
+/**
+ * @author Siddhant Sharma
+ * @date 06-Apr-2021 @time 11:39:48 pm
+ */
+public class Merge2List {
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(1);
+		l1.next = new ListNode(2);
+		l1.next.next = new ListNode(4);
+		
+		ListNode l2 = new ListNode(1);
+		l2.next = new ListNode(3);
+		l2.next.next = new ListNode(4);
+		
+		Merge2List obj = new  Merge2List();
+		ListNode result = obj.mergeTwoLists(l1, l2);
+		while(result!=null) {
+			System.out.println(result.val);
+			result = result.next;
+		}
+		
+	}
+	 public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		 
+		 PriorityQueue<Integer> queue = new PriorityQueue<>();
+		 
+		 while(l1!=null) {
+			 queue.offer(l1.val);
+			 l1 = l1.next;
+		 }
+		 while(l2!=null) {
+			 queue.offer(l2.val);
+			 l2 = l2.next;
+		 }
+		 
+		 ListNode result = new ListNode(queue.isEmpty()?null:queue.poll());
+		 ListNode tempResult = result;
+		 while(!queue.isEmpty()) {
+			 tempResult.next = new ListNode(queue.poll());
+			 tempResult = tempResult.next;
+		 }
+		 tempResult = null;
+		return result;
+	    }
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode() {}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
